b2cb7a55948b32a5c2142e162166fed66462d459
Евгений Сапрунов <sapra10062018@gmail.com>
2024-03-22 18:29:10 +0300

    Добавлены методы для поиска и преобразования для поиска по id и mono -> mono соответственно.

--- src/main/java/by/sapra/tasktrecker/user/service/UserService.java
+++ src/main/java/by/sapra/tasktrecker/user/service/UserService.java
@@ -2,7 +2,10 @@ package by.sapra.tasktrecker.user.service;
 
 import by.sapra.tasktrecker.user.service.model.UserModel;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface UserService {
     Flux<UserModel> getAll();
+
+    Mono<UserModel> getById(String id);
 }
--- src/main/java/by/sapra/tasktrecker/user/web/v1/mappers/ResponseMapper.java
+++ src/main/java/by/sapra/tasktrecker/user/web/v1/mappers/ResponseMapper.java
@@ -3,7 +3,10 @@ package by.sapra.tasktrecker.user.web.v1.mappers;
 import by.sapra.tasktrecker.user.service.model.UserModel;
 import by.sapra.tasktrecker.user.web.v1.model.UserResponse;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface ResponseMapper {
     Flux<UserResponse> userModelListToUserResponseList(Flux<UserModel> models);
+
+    Mono<UserResponse> monoModelToMonoResponse(Mono<UserModel> model);
 }
