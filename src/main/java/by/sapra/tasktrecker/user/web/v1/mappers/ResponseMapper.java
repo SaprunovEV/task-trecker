71495c16fd5489f20ae04ff2e5e4f575dd66e4aa
Евгений Сапрунов <sapra10062018@gmail.com>
2024-03-22 18:49:22 +0300

    #2_Создать_модуль_пользователя,_написать_к_нему_тесты:
    создан метод преобразующий request в модельку пользователя.

--- src/main/java/by/sapra/tasktrecker/user/web/v1/mappers/ResponseMapper.java
+++ src/main/java/by/sapra/tasktrecker/user/web/v1/mappers/ResponseMapper.java
@@ -1,6 +1,7 @@
 package by.sapra.tasktrecker.user.web.v1.mappers;
 
 import by.sapra.tasktrecker.user.service.model.UserModel;
+import by.sapra.tasktrecker.user.web.v1.model.UpsertUserRequest;
 import by.sapra.tasktrecker.user.web.v1.model.UserResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -9,4 +10,6 @@ public interface ResponseMapper {
     Flux<UserResponse> userModelListToUserResponseList(Flux<UserModel> models);
 
     Mono<UserResponse> monoModelToMonoResponse(Mono<UserModel> model);
+
+    UserModel requestToModel(UpsertUserRequest request);
 }
