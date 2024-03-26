93563e8f9a2fde801ac637aa38fdf81e519bba92
Евгений Сапрунов <sapra10062018@gmail.com>
2024-03-26 17:33:28 +0300

    #2_Создать_модуль_пользователя,_написать_к_нему_тесты:
    Написаны тесты для проверки на правильность работы поиска всех пользователей.

--- src/test/java/by/sapra/tasktrecker/user/service/UserServiceTest.java
+++ src/test/java/by/sapra/tasktrecker/user/service/UserServiceTest.java
@@ -1,20 +1,52 @@
 package by.sapra.tasktrecker.user.service;
 
+import by.sapra.tasktrecker.config.AbstractDataTest;
+import by.sapra.tasktrecker.user.model.UserModel;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Flux;
 
+import java.util.List;
+import java.util.stream.Stream;
+
+import static by.sapra.tasktrecker.testUtil.UserTestDataBuilder.aUser;
 import static org.junit.jupiter.api.Assertions.*;
-@ExtendWith(SpringExtension.class)
+
+@DataMongoTest
 @ContextConfiguration(classes = UserServiceConf.class)
-class UserServiceTest {
+class UserServiceTest extends AbstractDataTest {
     @Autowired
     UserService service;
+    @Test
+    void whenFindAll_thenReturnAllUsers() throws Exception {
+        List<String> expected = Stream.of(
+                testDbFacade.save(aUser().withName("name1")),
+                testDbFacade.save(aUser().withName("name2")),
+                testDbFacade.save(aUser().withName("name2")),
+                testDbFacade.save(aUser().withName("name3"))
+        ).map(UserModel::getName).sorted(String::compareTo).toList();
+
+        List<String> actual = service.getAll().toStream().map(UserModel::getName).sorted(String::compareTo).toList();
+
+        assertAll(() -> {
+            assertNotNull(actual);
+            assertFalse(actual.isEmpty());
+            assertEquals(expected.size(), actual.size());
+            for (int i = 0; i < expected.size(); i++) {
+                assertEquals(expected.get(i), actual.get(i));
+            }
+        });
+    }
 
     @Test
-    void shouldDoSomething() throws Exception {
+    void whenFindAll_andDatabaseIsEmpty_thenReturnEmptyFlux() throws Exception {
+        Flux<UserModel> actual = service.getAll();
 
+        assertAll(() -> {
+            assertNotNull(actual);
+            assertEquals(0, actual.toStream().count());
+        });
     }
 }
