5097626808baedcfe705642c4693cc8d413c47cf
Евгений Сапрунов <sapra10062018@gmail.com>
2024-03-26 11:57:13 +0300

    #2_Создать_модуль_пользователя,_написать_к_нему_тесты:
    Добавлены аннотации связанные с mongodb.

--- src/main/java/by/sapra/tasktrecker/user/model/UserModel.java
+++ src/main/java/by/sapra/tasktrecker/user/model/UserModel.java
@@ -3,11 +3,15 @@ package by.sapra.tasktrecker.user.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(collection = "usr")
 public class UserModel {
+    @Id
     private String id;
     private String name;
     private String email;
