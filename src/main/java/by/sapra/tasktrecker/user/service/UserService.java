e964f76c7fc98de7582073c7bf0dee1448e9dd40
Евгений Сапрунов <sapra10062018@gmail.com>
2024-03-26 11:56:02 +0300

    #2_Создать_модуль_пользователя,_написать_к_нему_тесты:
    Класс модели пользователя перенесён на пакет выше в связи с его использование как модель базы данных

--- src/main/java/by/sapra/tasktrecker/user/service/UserService.java
+++ src/main/java/by/sapra/tasktrecker/user/service/UserService.java
@@ -1,6 +1,6 @@
 package by.sapra.tasktrecker.user.service;
 
-import by.sapra.tasktrecker.user.service.model.UserModel;
+import by.sapra.tasktrecker.user.model.UserModel;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
