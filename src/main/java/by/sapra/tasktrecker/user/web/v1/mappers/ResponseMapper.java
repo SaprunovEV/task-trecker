f4fa3d79f2db4d47f9beb6c44aeebcdd6f1b089f
Евгений Сапрунов <sapra10062018@gmail.com>
2024-03-27 13:00:30 +0300

    #2_Создать_модуль_пользователя,_написать_к_нему_тесты:
    Настроен простейшее преобразование силами mapstruct.

--- src/main/java/by/sapra/tasktrecker/user/web/v1/mappers/ResponseMapper.java
+++ src/main/java/by/sapra/tasktrecker/user/web/v1/mappers/ResponseMapper.java
@@ -3,13 +3,22 @@ package by.sapra.tasktrecker.user.web.v1.mappers;
 import by.sapra.tasktrecker.user.model.UserModel;
 import by.sapra.tasktrecker.user.web.v1.model.UpsertUserRequest;
 import by.sapra.tasktrecker.user.web.v1.model.UserResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ResponseMapper {
-    Flux<UserResponse> userModelListToUserResponseList(Flux<UserModel> models);
+    default Flux<UserResponse> userModelListToUserResponseList(Flux<UserModel> models){
+        return models.map(this::userModelToUserResponse);
+    }
 
-    Mono<UserResponse> monoModelToMonoResponse(Mono<UserModel> model);
+    default Mono<UserResponse> monoModelToMonoResponse(Mono<UserModel> model) {
+        return model.map(this::userModelToUserResponse);
+    }
+
+    UserResponse userModelToUserResponse(UserModel userModel);
 
     UserModel requestToModel(UpsertUserRequest request);
 }
---
+++ src/test/java/by/sapra/tasktrecker/user/web/v1/mappers/ResponseMapperConf.java
@@ -0,0 +1,9 @@
+package by.sapra.tasktrecker.user.web.v1.mappers;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@TestConfiguration
+@ComponentScan(basePackages = "by.sapra.tasktrecker.user.web.v1.mappers")
+public class ResponseMapperConf {
+}
