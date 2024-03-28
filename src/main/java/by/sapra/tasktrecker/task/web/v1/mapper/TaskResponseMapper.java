13d370379355b791444e725c811a03b4f024b273
Евгений Сапрунов <sapra10062018@gmail.com>
2024-03-28 13:50:32 +0300

    #3 Создать модуль задач написать к нему тесты:
    Метод для преобразования запроса к серверу в модель задачи.

--- src/main/java/by/sapra/tasktrecker/task/web/v1/mapper/TaskResponseMapper.java
+++ src/main/java/by/sapra/tasktrecker/task/web/v1/mapper/TaskResponseMapper.java
@@ -2,6 +2,7 @@ package by.sapra.tasktrecker.task.web.v1.mapper;
 
 import by.sapra.tasktrecker.task.model.TaskModel;
 import by.sapra.tasktrecker.task.web.v1.model.TaskResponse;
+import by.sapra.tasktrecker.task.web.v1.model.TaskUploadRequest;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -9,4 +10,6 @@ public interface TaskResponseMapper {
     Flux<TaskResponse> fluxTaskModelToFluxTaskResponse(Flux<TaskModel> fluxTaskModel);
 
     Mono<TaskResponse> monoTaskModelToMonoTaskResponse(Mono<TaskModel> model);
+
+    Mono<TaskModel> monoRequestToModel(TaskUploadRequest request);
 }
