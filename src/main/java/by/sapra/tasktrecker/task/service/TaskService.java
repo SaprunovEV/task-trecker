82f8592c80999e1efb80a471347fafc2dba476a2
Евгений Сапрунов <sapra10062018@gmail.com>
2024-03-28 12:54:52 +0300

    #3 Создать модуль задач написать к нему тесты:
    Добавлены методы для получения по id и преобразования mono to mono для задач.

--- src/main/java/by/sapra/tasktrecker/task/service/TaskService.java
+++ src/main/java/by/sapra/tasktrecker/task/service/TaskService.java
@@ -2,7 +2,10 @@ package by.sapra.tasktrecker.task.service;
 
 import by.sapra.tasktrecker.task.model.TaskModel;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface TaskService {
     Flux<TaskModel> getAll();
+
+    Mono<TaskModel> getById(String id);
 }
--- src/main/java/by/sapra/tasktrecker/task/web/v1/mapper/TaskResponseMapper.java
+++ src/main/java/by/sapra/tasktrecker/task/web/v1/mapper/TaskResponseMapper.java
@@ -3,7 +3,10 @@ package by.sapra.tasktrecker.task.web.v1.mapper;
 import by.sapra.tasktrecker.task.model.TaskModel;
 import by.sapra.tasktrecker.task.web.v1.model.TaskResponse;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface TaskResponseMapper {
     Flux<TaskResponse> fluxTaskModelToFluxTaskResponse(Flux<TaskModel> fluxTaskModel);
+
+    Mono<TaskResponse> monoTaskModelToMonoTaskResponse(Mono<TaskModel> model);
 }
