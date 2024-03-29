dda28de6504456301e00e90299833c98ba5cf491
Евгений Сапрунов <sapra10062018@gmail.com>
2024-03-29 15:01:06 +0300

    #3 Создать модуль задач написать к нему тесты:
    Реализована чтение задачи.

--- src/main/java/by/sapra/tasktrecker/task/service/impl/MongoTaskService.java
+++ src/main/java/by/sapra/tasktrecker/task/service/impl/MongoTaskService.java
@@ -1,13 +1,24 @@
 package by.sapra.tasktrecker.task.service.impl;
 
 import by.sapra.tasktrecker.task.model.TaskModel;
+import by.sapra.tasktrecker.task.model.UserLinks;
+import by.sapra.tasktrecker.task.model.repo.TaskRepository;
+import by.sapra.tasktrecker.task.model.storade.TaskUserStorage;
 import by.sapra.tasktrecker.task.service.TaskService;
+import by.sapra.tasktrecker.user.model.UserModel;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class MongoTaskService implements TaskService {
+    private final TaskRepository repository;
+    private final TaskUserStorage storage;
     @Override
     public Flux<TaskModel> getAll() {
         return null;
@@ -15,7 +26,22 @@ public class MongoTaskService implements TaskService {
 
     @Override
     public Mono<TaskModel> getById(String id) {
-        return null;
+        return repository.findById(id).flatMap(task -> {
+            UserLinks userLinks = storage.getUserLinks(task);
+            return Mono.zip(
+                    Mono.just(task),
+                    userLinks.getAuthor(),
+                    userLinks.getAssignee(),
+                    Mono.zip(userLinks.getObservers(), (items) -> Arrays.stream(items).map(item -> (UserModel) item).collect(Collectors.toSet()))
+            ).map(tuple4 -> {
+                TaskModel result = tuple4.getT1();
+                result.setAuthor(tuple4.getT2());
+                result.setAssignee(tuple4.getT3());
+                result.setObservers(tuple4.getT4());
+
+                return result;
+            });
+        });
     }
 
     @Override
