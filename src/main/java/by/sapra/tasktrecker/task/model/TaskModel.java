08b4d84148701e2aaacd95b64b0a1297aaf69491
Евгений Сапрунов <sapra10062018@gmail.com>
2024-03-29 14:29:41 +0300

    #3 Создать модуль задач написать к нему тесты:
    Настроена сущность для записи в mongo.

--- src/main/java/by/sapra/tasktrecker/task/model/TaskModel.java
+++ src/main/java/by/sapra/tasktrecker/task/model/TaskModel.java
@@ -1,19 +1,22 @@
 package by.sapra.tasktrecker.task.model;
 
 import by.sapra.tasktrecker.user.model.UserModel;
-import by.sapra.tasktrecker.user.web.v1.model.UserResponse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.ReadOnlyProperty;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.Instant;
-import java.util.List;
 import java.util.Set;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(collection = "tsk")
 public class TaskModel {
+    @Id
     private String id;
     private String name;
     private String description;
@@ -22,7 +25,10 @@ public class TaskModel {
     private String authorId;
     private String assigneeId;
     private Set<String> observerIds;
+    @ReadOnlyProperty
     private UserModel author;
+    @ReadOnlyProperty
     private UserModel assignee;
+    @ReadOnlyProperty
     private Set<UserModel> observers;
 }
