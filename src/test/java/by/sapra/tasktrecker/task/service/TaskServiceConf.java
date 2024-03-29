87a83a2a51c885308aa42c147d29b23bbf349e1f
Евгений Сапрунов <sapra10062018@gmail.com>
2024-03-29 15:01:42 +0300

    #3 Создать модуль задач написать к нему тесты:
    Добавлено хранилище, из которого берутся пользователи.ю

--- src/test/java/by/sapra/tasktrecker/task/service/TaskServiceConf.java
+++ src/test/java/by/sapra/tasktrecker/task/service/TaskServiceConf.java
@@ -1,5 +1,7 @@
 package by.sapra.tasktrecker.task.service;
 
+import by.sapra.tasktrecker.task.model.repo.TaskRepository;
+import by.sapra.tasktrecker.task.model.storade.TaskUserStorage;
 import by.sapra.tasktrecker.task.service.impl.MongoTaskService;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -7,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 @TestConfiguration
 public class TaskServiceConf {
     @Bean
-    public TaskService service() {
-        return new MongoTaskService();
+    public TaskService service(TaskRepository repo, TaskUserStorage storage) {
+        return new MongoTaskService(repo, storage);
     }
 }
