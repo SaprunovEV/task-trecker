3564e5abcd45c6c3a5b116ce1313e309ca1dd169
Евгений Сапрунов <sapra10062018@gmail.com>
2024-04-05 15:42:10 +0300

    #3 Создать модуль задач написать к нему тесты:
    Удалена не нужная аннотация.

--- src/main/java/by/sapra/tasktrecker/task/model/storade/TaskUserStorage.java
+++ src/main/java/by/sapra/tasktrecker/task/model/storade/TaskUserStorage.java
@@ -2,9 +2,7 @@ package by.sapra.tasktrecker.task.model.storade;
 
 import by.sapra.tasktrecker.task.model.TaskModel;
 import by.sapra.tasktrecker.task.model.UserLinks;
-import org.springframework.stereotype.Component;
 
-@Component
 public interface TaskUserStorage {
     UserLinks getUserLinks(TaskModel task);
 }
